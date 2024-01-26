4718ea8da17bc399ceb2bff69c7a2aa43f180d93
Hassan Elsayed <hsaadelsayed@gmail.com>
2024-01-26 17:36:11 -0500

    finished learning nuxt project

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <p>{{ product.title }}</p>
-    <p>{{ product.price }}</p>
-    <p>{{ product.id }}</p>
+    <ProductDetails :product="product" />
   </div>
 </template>
 
 <script setup>
   const { id } = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
 
-  const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`, { key: id });
+  //  fetch the products
+  const { data: product } = await useFetch(uri, { key: id })
+
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+  }
 
   definePageMeta({
-    layout: 'products'
+    layout: "products",
   })
 </script>
-
-<style scoped>
-
-</style>
